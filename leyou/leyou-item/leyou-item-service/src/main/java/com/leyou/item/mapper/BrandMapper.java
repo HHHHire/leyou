c0d80dd5ea6074f7e89321653fee8421dbe2fb39
HHHHire <dengdh167@163.com>
2019-10-04 21:28:40 +0800

    add page

---
+++ leyou/leyou-item/leyou-item-service/src/main/java/com/leyou/item/mapper/BrandMapper.java
@@ -0,0 +1,12 @@
+package com.leyou.item.mapper;
+
+import com.leyou.item.pojo.Brand;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * @Author: ddh
+ * @Date: 2019/10/4 20:46
+ * @Description:
+ */
+public interface BrandMapper extends Mapper<Brand> {
+}
