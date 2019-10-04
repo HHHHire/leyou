a138d31e24e6f82858c6da20543034d04354590b
HHHHire <dengdh167@163.com>
2019-10-04 19:33:38 +0800

    nginx

---
+++ leyou/leyou-item/leyou-item-service/src/main/java/com/leyou/item/mapper/CategoryMapper.java
@@ -0,0 +1,12 @@
+package com.leyou.item.mapper;
+
+import com.leyou.item.pojo.Category;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * @author: ddh
+ * @date: 2019/10/4  18:59
+ * @description:
+ */
+public interface CategoryMapper extends Mapper<Category> {
+}
